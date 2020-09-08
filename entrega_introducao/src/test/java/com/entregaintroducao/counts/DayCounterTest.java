02bed5b3e3626a086bd99bcf980657fb02ee843c
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2020-09-08 10:51:02 -0300

    Test fails

--- entrega_introducao/src/test/java/com/entregaintroducao/counts/DayCounterTest.java
+++ entrega_introducao/src/test/java/com/entregaintroducao/counts/DayCounterTest.java
@@ -7,20 +7,19 @@ import org.junit.Test;
 public class DayCounterTest extends TestCase {
 
     @Test
-    public void testGetNumberOfDaysUntilOtherDate() {
-        LocalDate date = new LocalDate("2020-08-28");
-        LocalDate otherDate = new LocalDate("2020-09-07");
-        DayCounter dayCounter = new DayCounter(date);
-//        assertEquals(10, dayCounter.getNumberOfDaysUntilOtherDate(otherDate));
-        assertEquals(4, dayCounter.getNumberOfDaysUntilOtherDate(otherDate));
+    public void testGetNumberOfDaysBetweenTwoDates() {
+        LocalDate startDate = new LocalDate("2020-08-28");
+        LocalDate endDate = new LocalDate("2020-09-07");
+        DayCounter dayCounter = new DayCounter(startDate, endDate);
+        assertEquals("2020-08-28 - 2020-09-07 must be 10 days", 10, dayCounter.getNumberOfDaysBetweenTwoDates());
     }
 
     @Test
-    public void testGetNumberOfDaysUntilChristmas() {
-        LocalDate date = new LocalDate("2020-08-28");
-        LocalDate christmasDate = new LocalDate("2020-12-25");
-        DayCounter dayCounter = new DayCounter(date);
-//        assertEquals(119, dayCounter.getNumberOfDaysUntilChristmas(christmasDate));
-        assertEquals(0, dayCounter.getNumberOfDaysUntilChristmas(christmasDate));
+    public void testStartDateIsBeforeEndDate() {
+        LocalDate startDate = new LocalDate("2019-09-25");
+        LocalDate endDate = new LocalDate("2020-09-25");
+        DayCounter dayCounter = new DayCounter(startDate, endDate);
+        assertEquals("2019-09-25 - 2020-09-25 must be true", true, dayCounter.startDateIsBeforeEndDate());
     }
+
 }
